7f7407ccb868dfafd870603bcbfd3cf828bfaa48
Aiden Patel <123651777+aidenap21@users.noreply.github.com>
2025-04-04 21:07:31 -0500

    Create user_movie_list.py

---
+++ user_movie_list.py
@@ -0,0 +1,48 @@
+import random
+from letterboxdpy import user  as lb_user
+from letterboxdpy import movie as lb_movie
+
+class UserMovieList:
+    def __init__(self, username=None, movies=None):
+        # Find Letterboxd user
+        try:
+            self.user = lb_user.User(username)
+        except:
+            self.user = None
+            print(f"No user found for the username '{username}'")
+
+        # Add manually added movies to movie set
+        movie_set = set()
+        for movie in set(movies):
+            try:
+                movie_obj = lb_movie.Movie(movie)
+            except:
+                print(f"The film '{movie}' could not be found")
+                continue
+            
+            movie_set.add(movie_obj.title)
+
+        # Iterate through user's watched movies and add to movie set
+        if self.user is not None:
+            for movie in lb_user.user_films_watched(self.user):
+                movie_set.add(movie[1])
+
+        self.movies = list(movie_set)
+        self.movies.sort()
+
+    # Choose a given number of movies at random
+    def reduce_movies(self, count):
+        return random.sample(self.movies, count)
+
+
+if __name__ == "__main__":
+    extra_movies = ["Midsommar", "Black Swan", "Shutter Island", "sdfsfgda", "Django Unchained"]
+    uml = UserMovieList(username="aidenap21", movies=extra_movies)
+    reduced = uml.reduce_movies(10)
+    for i in reduced:
+        print(i)
+    
+        
+
+        
+    
