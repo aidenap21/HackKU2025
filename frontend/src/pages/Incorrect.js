ce785522f826d6eed9b60ca54a87a2bba5dcfde8
Aiden Patel <123651777+aidenap21@users.noreply.github.com>
2025-04-06 04:24:03 -0500

    Missed an import

--- frontend/src/pages/Incorrect.js
+++ frontend/src/pages/Incorrect.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, useLocation } from "react-router-dom";
 
 function Incorrect() {
   const navigate = useNavigate();
